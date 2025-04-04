2db446e122f52b09f31721e7ea12aecafbe71a6e
raavimanikanta <raavimani123@gmail.com>
2025-04-04 14:48:56 +0530

    added the npm hotjar package

--- src/App.js
+++ src/App.js
@@ -100,6 +100,14 @@ import Navbar from './components/Navbar';
 import StudentProvider from './components/Studentcontext';
 import Addstudent from './components/Addstudent';
 
+import { injectContentsquareScript } from '@contentsquare/tag-sdk';
+
+injectContentsquareScript({
+  siteId: "5361096",
+  async: true, // Optional: Set to false to wait for script execution until after document parsing.
+  defer: false // Optional: Set to true to defer script execution after document parsing.
+});
+
 // Component to track page views
 const TrackPageView = () => {
     const location = useLocation();
